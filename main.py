e98d2aa1874cd73c5792a8dbe4ba8da002ef6fd4
Paitius <kgebka1996@gmail.com>
2020-07-08 22:45:44 +0200

    wpisalem kolejny print f pod tutalem elo3

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-print("elo")
-
-print("elo3.200")
-
-print("hello")
+import pyodbc
+conn=pyodbc.connect('Driver={SQL Server};'
+                    'Server=LAPTOP-4Q8IGU5U;'
+                    'Database=nadzor_sprzedarzy_kamil;'
+                    'Trusted_Connection=yes;')
+cursor=conn.cursor()
+cursor.execute('SELECT * FROM nadzor_sprzedarzy_kamil.dbo.Klient_Kamil')
+for row in cursor:
+    print(row)
