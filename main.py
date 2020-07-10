38eb1fae0bb11e97bb561bc659d3f9f9b5a30900
Paitius <kgebka1996@gmail.com>
2020-07-10 17:53:27 +0200

    wpisalem kolejny print f pod tutalem elo3

--- main.py
+++ main.py
@@ -4,6 +4,24 @@ conn=pyodbc.connect('Driver={SQL Server};'
                     'Database=nadzor_sprzedarzy_kamil;'
                     'Trusted_Connection=yes;')
 cursor=conn.cursor()
-cursor.execute('SELECT * FROM nadzor_sprzedarzy_kamil.dbo.Klient_Kamil')
-for row in cursor:
-    print(row)
+
+def wyciaganie():
+    cursor.execute('SELECT * FROM nadzor_sprzedarzy_kamil.dbo.klient')
+    for list_database in cursor:
+        print(list_database)
+wyciaganie()
+
+
+def dodawanie(imie1, nazwisko1):
+    cursor.execute(f"INSERT INTO klient (imie, nazwisko) values ('{imie1}','{nazwisko1}')")
+    conn.commit()
+dodawanie("Pawelek","Kuzniak")
+wyciaganie()
+
+
+
+
+
+
+
+
